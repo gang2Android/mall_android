cb40b4899a8d0cefda9bac61aab3fef070706bf7
1729950804 <1729950804@qq.com>
2020-12-09 18:49:48 +0800

    1

--- test/src/main/java/com/gang/test/MainActivity.kt
+++ test/src/main/java/com/gang/test/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity_main)
+        setContentView(R.layout.activity_main)
 
 //        IntentUtils.getInstallAppIntent(this,"./")
 

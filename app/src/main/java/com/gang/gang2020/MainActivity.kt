5e643a172786ff19b32e8eb2af572caf8f685273
1729950804 <1729950804@qq.com>
2020-12-19 18:08:30 +0800

    1

--- app/src/main/java/com/gang/gang2020/MainActivity.kt
+++ app/src/main/java/com/gang/gang2020/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gang.gang2020
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openDY(view: View) {
+        val intent = Intent()
+        intent.data = Uri.parse("snssdk2329://user/profile/105069675259")
+//        intent.data = Uri.parse("snssdk0://")
+        startActivity(intent)
     }
 }
